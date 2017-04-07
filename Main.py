a797efec0c874128917a3d700471533dabfeac30
dakinfemiwa <daveak1602@gmail.com>
2017-04-07 23:19:20 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,15 @@ from tkinter import *
 root = Tk()
 root.geometry('1000x500')
 
-text
+#Constants
+rowLength = 10
+columnLength = 10
 
+for x in ro
 label = ttk.Label(text="Game")
 label.grid(row=0, column=0)
+
+label = ttk.Label(root, text=0)
+
+label for row in range(rowLength) for column in range(columnlength)
+
