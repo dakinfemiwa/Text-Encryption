dc19753aaf5e7707328428a87c3af8bff95b1e33
dakinfemiwa <daveak1602@gmail.com>
2017-04-26 21:58:06 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,37 @@
-import tkinter
-from tkinter import ttk
-from tkinter import *
+#IMPORTS
+#Imports random
+import random
+#Imports math
+import math
 
-root = Tk()
-root.geometry('1000x500')
+#BLANK VARIABLES
+#The variable which will have the 8 character key
+ascII_value = ""
+#This is for the sum of the numbers randomly chosen
+numbers = 0
 
-#Constants
-rowLength = 10
-columnLength = 10
+def ascII_converter():
+  global numbers, ascII_value
+  for digit in range(0, 8):
+    rand = random.randint(33, 126)
+    numbers = numbers + rand
+    character = chr(rand)
+    ascII_value += character
+  return ascII_value
 
-for x in ro
-label = ttk.Label(text="Game")
-label.grid(row=0, column=0)
+def off_set_value_calculator():
+  global numbers, off_set_value
+  for digit in range(0, 8):
+    numbers += ord(ascII_value[digit])
+  average = (numbers / 8)
+  average = math.floor(average)
+  off_set_value = average - 32
+  return off_set_value
+  
+#Perform functions  
+ascII_converter();
+off_set_value_calculator();
 
-label = ttk.Label(root, text=0)
-
-label for row in range(rowLength) for column in range(columnlength)
+#Displays the 8 cahracter code
+print("Your 8 character key is: \n\t{}".format(ascII_value))
 
